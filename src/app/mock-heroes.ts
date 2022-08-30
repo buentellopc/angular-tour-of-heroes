e80f92efd0554a822d4595e84daf90e11e27ecde
buentellopc <buentellopc@gmail.com>
2022-08-30 16:30:54 -0500

    Displaying authors

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 2, name: 'Eduardo Galeano' },
+  { id: 3, name: 'Ray Bradbury' },
+  { id: 4, name: 'Carl Sagan' },
+  { id: 5, name: 'Haruki Murakami' },
+  { id: 6, name: 'Ken Robinson' },
+  { id: 7, name: 'Frank Herbert' },
+  { id: 8, name: 'Octavio Paz' },
+  { id: 9, name: 'Milan Kundera' },
+  { id: 10, name: 'Marco Aurelio' },
+];
